c906cdea6c9f028efa7620aa90bd9e5d0e7af9a2
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-18 10:19:54 -0300

    chore: Criando projeto Spring Boot com Maven

---
+++ src/main/java/pro/anieldev/tecconn/TecconnApplication.java
@@ -0,0 +1,13 @@
+package pro.anieldev.tecconn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecconnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecconnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/anieldev/tecconn/TecconnApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.anieldev.tecconn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecconnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
