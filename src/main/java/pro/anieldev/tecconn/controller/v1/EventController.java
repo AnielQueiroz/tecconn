5edf65cd21e8c0c2a0cc47f388d5bf336633a3bb
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-18 12:15:40 -0300

    chore: Criando o EventController

---
+++ src/main/java/pro/anieldev/tecconn/controller/v1/EventController.java
@@ -0,0 +1,36 @@
+package pro.anieldev.tecconn.controller.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pro.anieldev.tecconn.model.Event;
+import pro.anieldev.tecconn.service.EventService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/events")
+public class EventController {
+    @Autowired
+    private EventService service;
+
+    @PostMapping("/new")
+    public Event addNewEvent(@RequestBody Event newEvent) {
+        return service.addNewEvent(newEvent);
+    }
+
+    @GetMapping("/")
+    public List<Event> getAllEvents() {
+        return service.getAllEvents();
+    }
+
+    @GetMapping("/find-by-pretty-name/{prettyName}")
+    public ResponseEntity<Event> getEventByPrettyName(@PathVariable String prettyName) {
+        Event evt = service.getByPrettyName(prettyName);
+
+        if (evt != null) {
+            return ResponseEntity.ok().body(evt);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
