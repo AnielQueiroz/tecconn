0b7d288389fb9f4ea843ebfce16ccdf2633bef72
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-21 13:16:18 -0300

    chore: Criando o SubscriptionController

---
+++ src/main/java/pro/anieldev/tecconn/controller/v1/SubscriptionController.java
@@ -0,0 +1,37 @@
+package pro.anieldev.tecconn.controller.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pro.anieldev.tecconn.dto.ErrorMessage;
+import pro.anieldev.tecconn.dto.SubscriptionResponse;
+import pro.anieldev.tecconn.exception.EventNotFoundException;
+import pro.anieldev.tecconn.exception.SubscriptionConflictException;
+import pro.anieldev.tecconn.model.User;
+import pro.anieldev.tecconn.service.SubscriptionService;
+
+@RestController
+@RequestMapping("/api/v1/subscriptions")
+public class SubscriptionController {
+    @Autowired
+    private SubscriptionService subsService;
+
+    @PostMapping("/create/{prettyName}")
+    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
+        try {
+            SubscriptionResponse res = subsService.createNewSubscription(prettyName, subscriber);
+
+            if (res != null) {
+                return ResponseEntity.ok(res);
+            }
+
+        } catch (EventNotFoundException ex) {
+            return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
+        }
+        catch (SubscriptionConflictException ex) {
+            return ResponseEntity.status(409).body(new ErrorMessage(ex.getMessage()));
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+}
