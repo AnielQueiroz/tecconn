7724e1212779f3979f50d2db9d69441acde36690
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-21 13:15:33 -0300

    chore: Criando o exceptions

---
+++ src/main/java/pro/anieldev/tecconn/exception/EventNotFoundException.java
@@ -0,0 +1,7 @@
+package pro.anieldev.tecconn.exception;
+
+public class EventNotFoundException extends RuntimeException{
+    public EventNotFoundException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/pro/anieldev/tecconn/exception/SubscriptionConflictException.java
@@ -0,0 +1,7 @@
+package pro.anieldev.tecconn.exception;
+
+public class SubscriptionConflictException extends RuntimeException{
+    public SubscriptionConflictException(String msg) {
+        super(msg);
+    }
+}
