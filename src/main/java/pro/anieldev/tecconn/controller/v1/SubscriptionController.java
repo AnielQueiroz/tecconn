ce078e9f2d62e5162f321d6f5c972ccebbe051d2
Aniel Queiroz <61893813+AnielQueiroz@users.noreply.github.com>
2025-02-21 14:43:35 -0300

    feat: Incrementando o controller para inscrição por indicação

--- src/main/java/pro/anieldev/tecconn/controller/v1/SubscriptionController.java
+++ src/main/java/pro/anieldev/tecconn/controller/v1/SubscriptionController.java
@@ -7,6 +7,7 @@ import pro.anieldev.tecconn.dto.ErrorMessage;
 import pro.anieldev.tecconn.dto.SubscriptionResponse;
 import pro.anieldev.tecconn.exception.EventNotFoundException;
 import pro.anieldev.tecconn.exception.SubscriptionConflictException;
+import pro.anieldev.tecconn.exception.UserIndicatorNotFoundException;
 import pro.anieldev.tecconn.model.User;
 import pro.anieldev.tecconn.service.SubscriptionService;
 
@@ -16,22 +17,22 @@ public class SubscriptionController {
     @Autowired
     private SubscriptionService subsService;
 
-    @PostMapping("/create/{prettyName}")
-    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber) {
+    @PostMapping({"/create/{prettyName}", "/create/{prettyName}/{userId}"})
+    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber, @PathVariable(required = false) Integer userId) {
         try {
-            SubscriptionResponse res = subsService.createNewSubscription(prettyName, subscriber);
+            SubscriptionResponse res = subsService.createNewSubscription(prettyName, subscriber, userId);
 
             if (res != null) {
                 return ResponseEntity.ok(res);
             }
 
-        } catch (EventNotFoundException ex) {
+        } catch (EventNotFoundException | UserIndicatorNotFoundException ex) {
             return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
         }
         catch (SubscriptionConflictException ex) {
             return ResponseEntity.status(409).body(new ErrorMessage(ex.getMessage()));
         }
 
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.badRequest().body(new ErrorMessage("Falha ao se inscrever no evento."));
     }
 }
